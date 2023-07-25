3ad776782fb9442bd95accdea85d03bde95cf7cb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-25 07:21:26 +0200

    a04: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A045F Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a04.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a04-user \
+    omni_a04-userdebug \
+    omni_a04-eng
